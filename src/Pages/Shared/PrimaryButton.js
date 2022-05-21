cc3ed82153de04e2bfb0b55fe72a64b4d491ec8d
sagor devnath <ssagor64@gmail.com>
2022-05-21 21:28:26 +0600

    folder structured

---
+++ src/Pages/Shared/PrimaryButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimaryButton = () => {
+    return (
+        <div>
+            <h1>This is Primary button</h1>
+        </div>
+    );
+};
+
+export default PrimaryButton;
