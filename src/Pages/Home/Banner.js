17fccd96be2beaae6609c6a3c976bcd465f97ed6
sagor devnath <ssagor64@gmail.com>
2022-05-26 11:47:53 +0600

    readme file update

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
       <>
       <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
+    <img src="https://i.ibb.co/25cBhgv/front-top.png" className="max-w-sm rounded-lg shadow-2xl" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
